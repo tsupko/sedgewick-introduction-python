4a2af147472f636dea09e893f5dc1fff646d9dde
a.tsupko <a.tsupko@innopolis.ru>
2017-12-24 20:40:01 +0300

    finished ch1 sec2 programs

---
+++ ch1/sec2/leapyear.py
@@ -0,0 +1,10 @@
+import sys
+from stdlib import stdio
+
+year = int(sys.argv[1])
+
+isLeapYear = (year % 4 == 0)
+isLeapYear = isLeapYear and ((year % 100) != 0)
+isLeapYear = isLeapYear or ((year % 400) == 0)
+
+stdio.writeln(isLeapYear)
